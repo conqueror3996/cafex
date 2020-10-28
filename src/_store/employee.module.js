bfd328872e410ac22e17c18031916e2f8c5ae6c7
my linh <lemylinh102@gmail.com>
2020-10-28 18:34:29 +0700

    Commit code employee.module

--- src/_store/employee.module.js
+++ src/_store/employee.module.js
@@ -1,25 +1,22 @@
 import { employeeService } from '../_services';
 import { router } from '../_helpers';
 
-const employee = JSON.parse(localStorage.getItem('user'));
-const state = employee
-    ? { status: { loggedIn: true }, employee, err: '' }
-    : { status: {}, employee: null, err: '' };
 
+const state = { status: {}, employee: null, err: '' };
 const actions = {
     login({ dispatch, commit }, { username, password }) {
         commit('loginRequest', { username });
-    
         employeeService.login(username, password)
             .then(
                 info => {
-                    console.log(info)
-                    if (info.data) {
-                        if (info.data.code) {
-                            dispatch('alert/error', info.data.code, { root: true });
-                            commit('loginFailure', info)
+                    if (info) {
+                        if (info.error.code) {
+                            dispatch('alert/error', info.error.code, { root: true });
+                            commit('loginFailure', info.error);
                         } else {
-                            commit('loginSuccess', info)
+                            commit('loginSuccess', info);
+                            this.userInfo();
+                            //check permission (first login, manager, sale)
                             // router.push('/WA01010300');
                         }
                     }
@@ -29,9 +26,8 @@ const actions = {
     },
     logout({ commit }) {
         employeeService.logout().then((info) => {
-            if (info.data.code) {
-                dispatch('alert/error', info.data.code, { root: true });
-                // commit('getAllFailure', info)
+            if (info.error.code) {
+                dispatch('alert/error', info.error.code, { root: true });
             } else {
                 commit('logout');
                 auth.clearAuthToken();
@@ -39,45 +35,18 @@ const actions = {
             // remove user from local storage to log user out
         });
     },
-    register({ dispatch, commit }, employee) {
-        commit('registerRequest', employee);
-    
-        employeeService.register(employee)
-            .then(
-                employee => {
-                    commit('registerSuccess', employee);
-                    router.push('/WA01010100');
-                    setTimeout(() => {
-                        // display success message after route change completes
-                        dispatch('alert/success', 'Registration successful', { root: true });
-                    })
-                },
-                error => {
-                    commit('registerFailure', error);
-                    dispatch('alert/error', error, { root: true });
-                }
-            );
-    },
     changePassword({commit}, input) {
         
         employeeService.changePassword(input).then(
             info => {
-                if (info.data) {
-                    if (info.data.code) {
-                        dispatch('alert/error', info.data.code, { root: true });
-                        commit('changePasswordFailed', info.data);
-                        dispatch('alert/error', info.data.code, { root: true });
+                if (info) {
+                    if (info.error) {
+                        dispatch('alert/error', info.error.code, { root: true });
+                        commit('changePasswordFailed', info.error);
                     } else {
-                        // commit('changePasswordSuccess', employee);
                         router.push('/WA01010201');
-                        // Xóa hết dữ liệu
-                        setTimeout(() => {
-                            // display success message after route change completes
-                            dispatch('alert/success', 'Change Password successful', { root: true });
-                        })
                     }
                 }
-                
             },
             error => {
                 commit('changePasswordFailed', error);
@@ -85,6 +54,23 @@ const actions = {
             }
         );
 
+    },
+    userInfo({commit}){
+        employeeService.getEmployeeUserinfo().then(
+            info => {
+                if(info){
+                    if(info.error.code){
+                        dispatch('alert/error', info.error.code, { root: true });
+                        commit('getUserInfoFailed', info.error);
+                    }else{
+                        commit("getUserInfoSuccess", info);
+                    }
+                }
+            },
+            error => {
+                commit("getUserInfoFailed", error);
+            }
+        )
     }
 };
 
@@ -106,18 +92,18 @@ const mutations = {
         state.status = {};
         state.employee = null;
     },
-    registerRequest(state, employee) {
-        state.status = { registering: true };
-    },
-    registerSuccess(state, employee) {
+    changePasswordFailed(state, error){
         state.status = {};
+        state.err = error;
     },
-    registerFailure(state, error) {
+    getUserInfoSuccess(state, employee){
         state.status = {};
+        state.employee = employee;
+    },
+    getUserInfoFailed(state, error){
+        state.status = {};
+        state.err = error;
     }
-    // changePasswordSuccess(state) {
-
-    // }
 };
 
 export const employees = {
