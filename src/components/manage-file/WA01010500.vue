00aa9c69b2d6dccea8841a02381911ade8e3e6bf
my linh <lemylinh102@gmail.com>
2020-11-23 10:48:24 +0700

    Commit fix screen WA01010500

--- src/components/manage-file/WA01010500.vue
+++ src/components/manage-file/WA01010500.vue
@@ -89,8 +89,10 @@
            <img v-show="isUploading" src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
         </div>
       </div>
-      <div class="panel">
-        <img :src="this.responseImage">
+      <div class="panel box-imgShow">
+        <div class="box-show">
+          <img :src="this.responseImage" class="mw100 imgShow">
+        </div>
       </div>
     </div>
   </div>
@@ -287,6 +289,9 @@ export default {
   max-width: 1344px;
   margin: 0 auto;
 }
+.imgShow{max-height: 480px;background: #fff;}
+.box-show{display:inline-block;}
+.box-imgShow{text-align:center;display:flex;flex-direction:column;justify-content: center;align-content: center;}
 /* .manage-file-conent {
   display: flex;
   justify-content: left;
